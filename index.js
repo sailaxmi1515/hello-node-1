45eb73d133dbd3ad6eeb04340a7b8e50d120a380
vicky8055 <84907267+vicky8055@users.noreply.github.com>
2022-11-16 20:45:09 +0530

    MY FIRSTCOMMIT

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = request("http");
+const fs = require("fs");
+const server = http.createServer((req,res) => {
+    fs.readFile("sample.txt",(err,data) => {
+        res.end(data);
+    });
+});
+server.listen(3000);
